b2699fed7ca834de8679c00bae406ccdad3cc9d5
Scott Bamford <scott@ambidect.company>
2019-07-08 13:16:49 +0100

    Import code to be opensourced under MIT license.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pojo-validator-react
 React hook that makes using pojo-validator easy with react.
+
+## Documentation
+
+This is a child project of [pojo-validator](https://github.com/scottbamford/pojo-validator#readme) to provide react support.
+
+Full documentation is available at: https://github.com/scottbamford/pojo-validator#readme](https://github.com/scottbamford/pojo-validator#readme)
---
+++ src/useValidatorCallback.ts
@@ -0,0 +1,34 @@
+﻿import * as React from "react";
+import { ValidationErrors, Validator, ValidationCallback, ValidationState } from "pojo-validator";
+
+/**
+ * Hook that provides a validate method and its associated validationErrors as a state object managed by validation.
+ * 
+ * validation is a callback that is working on a known model, with appropriate deps supplied.  This means it does not receive
+ * a model to be checked as a passed in argument in the same way useValidator() does.
+ * 
+ * @param validating the validation method.
+ */
+export function useValidatorCallback(validating: (validation: ValidationState, fieldsToCheck?: Array<string>) => void, deps: React.DependencyList): [(fieldsToCheck?: Array<string>) => boolean, ValidationErrors] {
+    const [validationErrors, setValidationErrors] = React.useState<ValidationErrors>({});
+
+    // Create a ValidationCallback that ignores the model it receives and uses model that we get passed in as a dependency.
+    const validatingModel = React.useCallback((model: any, validation: ValidationState, fieldsToCheck?: Array<string>): void => {
+        return validating(validation, fieldsToCheck);
+    }, deps);
+
+    // Create the method in the right format for us to return it so we can call validate() directly.
+    const validate = React.useCallback((fieldsToCheck?: Array<string>): boolean => {
+        let validator = new Validator(validatingModel);
+        const ok = validator.validate({} /* Model is ignored by our validation code so we pass an empty object */, fieldsToCheck);
+
+        setValidationErrors({
+            ...validationErrors,
+            ...validator.errors()
+        });
+
+        return ok;
+    }, [...deps, validatingModel, validationErrors, setValidationErrors]);
+
+    return [validate, validationErrors];
+}
